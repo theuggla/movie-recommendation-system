f4a53f02f251375be994431ca316a8466e9248f9
Molly Arhammar <mollyarhammar@gmail.com>
2018-11-20 15:24:36 +0100

    complete kmeans algorithm

--- server/server.py
+++ server/server.py
@@ -7,19 +7,39 @@ from json import dumps
 from flask_jsonpify import jsonify
 from flask_cors import CORS
 
+# Local imports
+from utils import Util
+from kmeans_clustering import KMeansCluster
+
 # Global definitions
 app = Flask(__name__)
 api = Api(app)
 CORS(app)
 
-# Get the users
+# Root
 class Root(Resource):
     def get(self):
-        result = {'message': 'connected'}
+        filename = './blogdata.txt'
+        result = {'message': Util.readfile(filename)}
         return jsonify(result)
+
+# Get the clusters
+class Clusters(Resource):
+    def get(self, iterations):
+        filename = './blogdata.txt'
+        data = Util.readfile(filename)
+
+        try:
+            number_of_iterations = int(iterations)
+        except ValueError:
+            number_of_iterations = False
+        finally:
+            clusters = KMeansCluster.run_assignments(data, 5, number_of_iterations)
+            return jsonify([[assignment['blog'] for assignment in cluster.assignments] for cluster in clusters])
 
 # Add resources
 api.add_resource(Root, '/') # Route_1
+api.add_resource(Clusters, '/clusters/kmeans/<iterations>') # Route_2
 
 
 # Start server
